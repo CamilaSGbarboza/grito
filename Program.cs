1537165b471180b2bfe63d2bd7c124aa2f4b5146
camila <camilasangobar@gmail.com>
2021-03-12 14:34:27 -0300

    grito

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace grito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite algo");
+            string resposta = Console.ReadLine();
+            Console.WriteLine($"{resposta.ToUpper()}");
+        }
+    }
+}
